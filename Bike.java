b1858464777f3de6d73fe3495491e049cce9b7c0
blakshmi19 <133948987+blakshmi19@users.noreply.github.com>
2023-05-18 13:02:36 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Bike {
+
+	public void driveBike() {
+		System.out.println("drive bike"); 
+	}
+	public void applyBrake() {
+		System.out.println("applyBrake"); 
+	}
+	public void soundHorn() {
+		System.out.println("soundHorn"); 
+	}
+	public void isPuncture() {
+		System.out.println("isPuncture"); 
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bike b = new Bike();
+		b.driveBike();
+		b.applyBrake();
+		b.soundHorn();
+		b.isPuncture();
+
+	}
+
+}
---
+++ Smartphone.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iphone I = new Iphone();
+		I.makecall();
+		I.model();
+				
+		
+	}
+
+}
